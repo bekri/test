cf2e415138ff7f8131207ed0aa416af726d2f5ae
bekri <babro.koy.kari@gmail.com>
2023-09-24 20:02:02 +0100

    changes

--- scripts/password.js
+++ scripts/password.js
@@ -1,28 +1,22 @@
-// password.js
+// password.js (server-side script)
 
-function checkPassword() {
-    const passwordInput = document.getElementById("passwordInput").value.trim(); // Trim whitespace
+const { VercelSecrets } = require('@vercel/secrets');
 
-    // Make an HTTP POST request to the serverless function for password validation
-    fetch('/api/passwordValidation', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ passwordInput }),
-    })
-    .then(response => {
-        if (response.status === 200) {
-            // Password is correct, set authorization flag and redirect
-            const expirationTime = new Date().getTime() + 30 * 60 * 1000; // 30 minutes in milliseconds
-            localStorage.setItem('authorized', JSON.stringify({ authorized: true, expiration: expirationTime }));
-            window.location.href = "data.html";
+module.exports = async (req, res) => {
+    const secrets = new VercelSecrets();
+    const { PASSWORD } = await secrets.extract();
+
+    try {
+        const body = JSON.parse(req.body);
+        const enteredPassword = body.password;
+
+        if (enteredPassword === PASSWORD) {
+            res.json({ accessGranted: true });
         } else {
-            // Incorrect password, display an error message
-            alert("Incorrect password. Please try again.");
+            res.json({ accessGranted: false });
         }
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
+    } catch (error) {
+        console.error('Error checking password:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
