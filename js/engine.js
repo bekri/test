ededab60ceb2e14f1ad8cd438275fe5b61dd6b0a
bekri <babro.koy.kari@gmail.com>
2023-09-24 23:01:43 +0100

    test fuclk

---
+++ js/engine.js
@@ -0,0 +1,26 @@
+function checkPassword() {
+    const passwordInput = document.getElementById("passwordInput").value;
+
+    // Make a POST request to the server for authentication
+    fetch("/check-password", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ password: passwordInput })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.authorized) {
+            // Password is correct, set authorization flag and redirect
+            localStorage.setItem('authorized', 'true');
+            window.location.href = "data.html"; // Replace with the actual main page URL
+        } else {
+            alert("Incorrect password. Please try again.");
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again later.");
+    });
+}
