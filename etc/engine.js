87225764cb10ae0298f70b3197fb9cefba456d0d
bekri <babro.koy.kari@gmail.com>
2023-09-24 22:44:22 +0100

    backto0

--- etc/engine.js
+++ etc/engine.js
@@ -1,15 +1,11 @@
 function checkPassword() {
-
     const password = "@150564220493"; // Replace with your actual password
-
-// Rest of your code remains the same
-
     const passwordInput = document.getElementById("passwordInput").value;
 
-    if (passwordInput === storedPassword) {
+    if (passwordInput === password) {
         // Password is correct, set authorization flag and redirect
         localStorage.setItem('authorized', 'true');
-        window.location.href = "data.html"; // Replace with the actual main page URL
+        window.location.href = "connect.html"; // Replace with the actual main page URL
     } else {
         alert("Incorrect password. Please try again.");
     }
